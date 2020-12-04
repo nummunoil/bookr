ab6904690a653e56a858e23991e1a571a8f258f2
Oil Sudarat <sudarat@enres.co>
2020-12-04 23:59:51 +0700

    Add BookTransformer

--- app/Transformer/BookTransformer.php
+++ app/Transformer/BookTransformer.php
@@ -6,4 +6,21 @@ use League\Fractal\TransformerAbstract;
 
 class BookTransformer extends TransformerAbstract
 {
+    /**
+    * Transform a Book model into an array
+    *
+    * @param Book $book
+    * @return array
+    */
+    public function transform(Book $book)
+    {
+        return [
+            'id' => $book->id,
+            'title' => $book->title,
+            'description' => $book->description,
+            'author' => $book->author,
+            'created' => $book->created_at->toIso8601String(),
+            'updated' => $book->updated_at->toIso8601String(),
+        ];
+    }
 }
