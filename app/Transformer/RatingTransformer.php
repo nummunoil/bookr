a8baa6221920734defccb68f3500a56ea9f29f23
Oil Sudarat <sudarat@enres.co>
2021-05-31 00:20:41 +0700

    Adding an Author Rating

---
+++ app/Transformer/RatingTransformer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Transformer;
+
+use App\Rating;
+
+use League\Fractal\TransformerAbstract;
+
+/**
+* Class RatingTransformer
+* @package App\Transformer
+*/
+class RatingTransformer extends TransformerAbstract
+{
+
+    /**
+    * Transform a Rating
+    *
+    * @param Rating $rating
+    * @return array
+    */
+    public function transform(Rating $rating)
+    {
+        return [
+            'id' => $rating->id,
+            'value' => $rating->value,
+            'type' => $rating->rateable_type,
+            'links' => [
+                [
+                    'rel' => 'author',
+                    'href' => route('authors.show', ['id' => $rating->rateable_id])
+                ]
+            ],
+            'created' => $rating->created_at->toIso8601String(),
+            'updated' => $rating->updated_at->toIso8601String(),
+        ];
+    }
+}
